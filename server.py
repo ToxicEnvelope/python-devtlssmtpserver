c3376ce0976a367e6e58d48d22d3c4f597848e84
Thomas Spycher <tspycher@zerodine.com>
2013-09-19 10:29:47 +0200

    Minor enhancements to make the class more easy to use when used in an unittest

--- server.py
+++ server.py
@@ -1,19 +1,23 @@
-from OpenSSL import SSL
 from twisted.internet import reactor, ssl
 from twisted.internet.protocol import ServerFactory
 from twisted.protocols.basic import LineReceiver
 
+
+class TLSFactory(ServerFactory):
+    mailcontent = []
+    communication = []
+
+
 class TLSServer(LineReceiver):
     inData = False
-    mailcontent = []
+
     def connectionMade(self):
         self.sendLine("220 this.mx ESMTP Dev Server")
     
     def lineReceived(self, line):
-        print "received: " + line   
+        self.factory.communication.append(line)
 
         if "ehlo" in line:
-            #conn.send("250 OK\r\n")
             self.sendLine("250-this.mx at your service")
             self.sendLine("250-SIZE 35882577")
             self.sendLine("250-8BITMIME")
@@ -35,28 +39,37 @@ class TLSServer(LineReceiver):
         if line == "quit":
             self.sendLine("221 2.0.0 Bye")
             self.transport.loseConnection()
-            print "Mail Received:\n"
-            print "\n".join(self.mailcontent)
-            return
+            reactor.stop()
 
         if not "data" in line:
             if not self.inData:
                 self.sendLine("250 OK")
-                print "--- 250 OK"
                 return
 
             if line == ".":
                 self.sendLine("250 2.0.0 OK, MESSAGE ACCEPTED")
             else:
-                self.mailcontent.append(line)
+                self.factory.mailcontent.append(line)
         else:
             self.sendLine("354 End data with <CR><LF>.<CR><LF>")
             print "--- 354 End data with <CR><LF>.<CR><LF>"
             self.inData = True
-            #receivingData = True
+
+
+class SMTPDevServer(object):
+    port = None
+
+    def __init__(self, port=102525):
+        self.port = port
+
+    def receiveOneMail(self):
+        factory = TLSFactory()
+        factory.protocol = TLSServer
+        con = reactor.listenTCP(self.port, factory)
+        reactor.run()
+        con.stopListening()
+        return {"communication": factory.communication, "rawmail": factory.mailcontent}
 
 if __name__ == '__main__':
-    factory = ServerFactory()
-    factory.protocol = TLSServer
-    reactor.listenTCP(2525, factory)
-    reactor.run()
+    smtp = SMTPDevServer(port=2525)
+    comm, mail = smtp.receiveOneMail()
